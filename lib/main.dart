650ff3f767ac0a905547212481394f13a7d4a6b6
Netall0 <113532176+Netall0@users.noreply.github.com>
2025-03-24 23:47:06 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://yqgxbbzdopsnogdfgqag.supabase.co',
+    url: '',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxZ3hiYnpkb3Bzbm9nZGZncWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NzcxMjYsImV4cCI6MjA1NTU1MzEyNn0.TIZ271LKR06yRr594wr976f_kEU0fJoHGY-PYDdOC4w',
+        '',
   );
 
   runApp(MaterialApp(home: AuthGuard()));
